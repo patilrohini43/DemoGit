00001a842edca75416018ab74d34ddab075c2f3d
rohini <patilrohini43@gmail.com>
2021-08-20 19:15:29 +0530

    Added:logic for sub

--- src/com/bl/demogit/Demo.java
+++ src/com/bl/demogit/Demo.java
@@ -4,10 +4,10 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome");
-		int a=10;
-		int b=20;
+		int a=20;
+		int b=10;
 		int sum=a+b;
+		int sub=a-b;
 		System.out.println(sum);
-	}
-
+ }
 }
